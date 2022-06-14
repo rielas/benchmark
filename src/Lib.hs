5f782958b09b12820ff45de882db418a1cff9493
Anatol Karalkoŭ <anatol.karalkou@brightsec.com>
2022-06-14 07:39:27 +0000

    Print statistics

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,13 +13,12 @@ module Lib
 where
 
 import Data.Aeson
-import Data.ByteString hiding (pack, putStrLn)
+import Data.ByteString hiding (pack, putStrLn, putStr)
 import Data.ByteString.Builder (toLazyByteString)
 import Data.Maybe (isJust)
 import Data.Text (Text, breakOn, breakOnEnd, dropAround, pack)
 import Data.Text.Encoding
 import Data.Text.Encoding (encodeUtf8Builder)
-import Data.Text.IO (putStrLn)
 import Data.Text.Read
 import Data.Time
 import Data.Time.Clock
@@ -27,8 +26,9 @@ import Debug.Trace
 import GHC.Generics
 import GHC.IO
 import System.Exit
+import Text.Printf
 import Text.Show (Show, show)
-import Prelude hiding (Show, drop, dropWhile, getLine, length, putStrLn, show)
+import Prelude hiding (Show, drop, dropWhile, getLine, length, show)
 
 data Snapshot = Snapshot
   { timestamp :: Double,
@@ -44,6 +44,15 @@ data Status = Status
   deriving (Generic, Show, Eq)
 
 instance FromJSON Status
+
+slices = [2, 3, 5, 8, 13, 21, 34, 55]
+
+data Stats = Stats
+  {  requestsFor2Mins :: Integer,
+     entrypointsFor2Mins :: Integer,
+     requestsFor3Mins :: Integer,
+     entrypointsFor3Mins :: Integer
+  }
 
 process :: ByteString -> Maybe Snapshot
 process record = do
@@ -78,11 +87,22 @@ getElapsedTime :: Status -> Maybe DiffTime
 getElapsedTime status =
   parseTimeM True defaultTimeLocale "%H:%M:%S" (elapsed status)
 
+printStats :: Snapshot -> String
+printStats snapshot =
+  let elapsed' = elapsed . status $ snapshot
+      requests' = requests . status $ snapshot
+      entry_points' = entry_points . status $ snapshot
+   in printf
+          "%s requests: %6d,  entry points: %5d"
+          elapsed'
+          requests'
+          entry_points'
+
 mainIo :: IO b
 mainIo = do
   line <- getLine
   let stats = process line
   case stats of
     Nothing -> pure ()
-    Just stats -> putStrLn (pack $ show stats)
+    Just stats -> putStrLn $ printStats stats
   mainIo
